e8837cd1489a32b2297df8283d715a72ded77590
Henry Oberholtzer <henryoberholtzer@gmail.com>
2023-10-20 10:38:05 -0700

    made js functional

--- js/scripts.js
+++ js/scripts.js
@@ -2,38 +2,39 @@
 
 
 // // Question Retrieval
-const uR = document.querySelector('input[name="uR"]:checked').value;
-const oE = document.querySelector('input[name="oE"]:checked').value;
-const sD = document.querySelector('input[name="sD"]:checked').value;
-const fB = document.querySelector('input[name="uR"]:checked').value;
-const mA = document.querySelector('input[name="mA"]:checked').value;
-console.log(uR);
-console.log(oE);
 
 function suggestLanguage(e) {
     e.preventDefault();
-    let resultText = "";
-    if (uR === "r") {
-        if (oE === "o") {
-            resultText = "Ruby";
-        } else if (sD === "s") {
-            resultText = "Go";
-        } resultText = "Python"
-    } else if (oE === "e") {
-        resultText = "Rust";
-    } else if (sD === "d") {
-        resultText = "JavaScript";
-    } else if (fB === "f") {
-        resultText = "JavaScript";
-    } else if (mA === "m") {
-        resultText = "C#";
-    } resultText = "Swift";
-    document.getElementById("resultText").innerText = resultText;
+
+    const uR = document.querySelector("input[name='uR']:checked").value;
+    const oE = document.querySelector("input[name='oE']:checked").value;
+    const sD = document.querySelector("input[name='sD']:checked").value;
+    const fB = document.querySelector("input[name='uR']:checked").value;
+    const mA = document.querySelector("input[name='mA']:checked").value;
+
+    function questionBranches(uR, oE, mA, sD, fB) {
+        if (uR === "r") {
+            if (oE === "o") {
+                return "Ruby";
+            } else if (sD === "s") {
+                return "Go";
+            } return "Python"
+        } else if (oE === "e") {
+            return "Rust";
+        } else if (sD === "d") {
+            return "JavaScript";
+        } else if (fB === "f") {
+            return "JavaScript";
+        } else if (mA === "m") {
+            return "C#";
+        } return "Swift";
+    };
+    document.getElementById("resultText").innerText = questionBranches(uR, oE, mA, sD, fB);
 }
 
 // UI Logic
 
-window.addEventListener("load", function() {
+window.addEventListener("load", function () {
     const form = document.getElementById("form");
     form.addEventListener("submit", suggestLanguage);
 });
